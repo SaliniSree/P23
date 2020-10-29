b882cee5154b8ba459316cadc79d8ef7909c91d9
SaliniS <salinisree03@gmail.com>
2020-10-29 10:33:34 +0530

    sketch edited

--- sketch.js
+++ sketch.js
@@ -39,26 +39,26 @@ function setup() {
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
- 	boxPosition=width/2-100
- 	boxY=610;
+ 	var boxXpos=width/2,
+ 	boxYpos=650;
 
 
- 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite=createSprite(boxXpos-100, boxYpos-45, 20,100);
  	boxleftSprite.shapeColor=color(255,0,0);
 
- 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	boxLeftBody = Bodies.rectangle(boxXpos-100, boxYpos-45, 20,100 , {isStatic:true} );
  	World.add(world, boxLeftBody);
 
- 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase=createSprite(boxXpos, boxYpos, 200,20);
  	boxBase.shapeColor=color(255,0,0);
 
- 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	boxBottomBody = Bodies.rectangle(boxXpos, boxYpos, 200,20 , {isStatic:true} );
  	World.add(world, boxBottomBody);
 
- 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
+ 	boxrightSprite=createSprite(boxXpos+100 , boxYpos-45, 20,100);
+ 	boxrightSprite.shapeColor=color(255,0,0);
 
- 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	boxRightBody = Bodies.rectangle(boxXpos+100 , boxYpos-45, 20,100 , {isStatic:true} );
  	World.add(world, boxRightBody);
 
 
